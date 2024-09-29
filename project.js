5a88f166da06f6b65aff878c17ed310868058d30
Duncan Byrne <duncanbyrne69@outlook.com>
2024-09-29 18:27:38 -0400

    added function to put in deposit amount

---
+++ project.js
@@ -0,0 +1,20 @@
+// 1. deposit money
+// 2. determine number of betting lines
+// 3. collect bet amount
+// 4. spin machine
+// 5.check if user won 
+// 6. give user their money
+// 7. play again
+
+// function deposit(){
+//   return 1
+// }
+
+const prompt = require('prompt-sync')();
+
+const deposit = () => {
+  const depositAmount = prompt('Enter deposit amount: ')
+
+}
+
+deposit();
